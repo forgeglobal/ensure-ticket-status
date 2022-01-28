76469c8606f6b6c47cc7975749425c92b0d43f1e
Ruben Aguilar <ruben.aguilar@forgeglobal.com>
2022-01-28 12:44:28 -0800

    From origin

--- src/ensure_ticket_status_on_pull_request.rb
+++ src/ensure_ticket_status_on_pull_request.rb
@@ -72,7 +72,7 @@ compare_branch = ENV["INPUT_PULL_REQUEST_COMPARE_BRANCH_NAME"]
 base_branch = ENV["INPUT_PULL_REQUEST_BASE_BRANCH_NAME"]
 
 # parse ticket id out of branch
-commits = `git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative #{base_branch}..#{compare_branch}`
+commits = `git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative origin/#{base_branch}..origin/#{compare_branch}`
 puts commits
 
 ticket_id = "SHAR-1365" # strip off pr branch
